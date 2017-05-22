1790b782f84dec9068c0e0e52e558b826847e4eb
Albert Abril <albert.abril@gmail.com>
2017-05-22 22:02:03 +0200

    avoid to load esling on webpack due to fsevents linux warning

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,13 +26,15 @@ module.exports = {
     ** Run ESLINT on save
     */
     extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+      if(process.env.NODE_ENV==='production'){
+        if (ctx.isClient) {
+          config.module.rules.push({
+            enforce: 'pre',
+            test: /\.(js|vue)$/,
+            loader: 'eslint-loader',
+            exclude: /(node_modules)/
+          })
+        }
       }
     }
   }
