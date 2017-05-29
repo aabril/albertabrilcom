87f6133ace5f22af40f7a5c5c7b8fdc731669a10
Albert Abril <albert.abril@gmail.com>
2017-05-29 23:59:21 +0200

    remove useless comment

--- pages/root.vue
+++ pages/root.vue
@@ -39,7 +39,7 @@ export default {
 .fade-enter-active, .fade-leave-active {
   transition: opacity 2.4s
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+.fade-enter, .fade-leave-to {
   opacity: 0
 }
 </style>
